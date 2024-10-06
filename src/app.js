7352c93ff805ff1caf24b3208c64ae547d00bbef
webdev-ashishk <webdev.ashishk@gmail.com>
2024-10-06 14:32:34 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Welcome to Test API project ...');
+});
+app.listen(port, () => {
+  console.log(`server is running on port: ${port}`);
+});
